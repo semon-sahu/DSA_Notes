3685bcf55c134929042af4494b9a80eb1d10059d
Semon Sahu <111222semonsahu@gmail.com>
2025-02-22 13:38:49 +0530

    template

---
+++ template/tempalte.cpp
@@ -0,0 +1,20 @@
+// templet is a generlayzed formate of  programming that may be  a function or class 
+// there are two type of template 
+// 1.Funtion template
+// 2.class templet
+
+// <template t1> :content place holder
+#include<iostream>
+using namespace std;
+template<typename t1>
+t1 sun(t1 a,t1 b){
+    return a+b;
+}
+template<typename x1,typename x2>
+x1 myltiply(x1 x,x2 y){
+    return x*y;
+}
+int main(){
+    cout<<sun(5,6)<<endl;
+    cout << myltiply(3, 2.3);
+}
