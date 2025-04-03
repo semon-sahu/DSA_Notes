c3913cee4d05aa0cc4a99a02d0da3fa69d88e252
Semon Sahu <111222semonsahu@gmail.com>
2025-04-03 23:54:47 +0530

    change

--- valid-syntex/Validsyntex1.cpp
+++ valid-syntex/Validsyntex1.cpp
@@ -1,3 +1,55 @@
+//#include<iostream>
+//using namespace std;
+//#include<stack>
+//bool valid(string s)
+//
+//{
+//    stack<char>st;
+//    bool r=true;
+//    for(int i=0;i<s.size();i++){
+//        if(s[i]=='['|| s[i]=='{'|| s[i]=='('){
+//            st.push(S[i]);
+//
+//        }
+//        else if(s[i]==']'){
+//            if(! st.empty()&& st.top()=='['){
+//                st.pop();
+//            }
+//            else{
+//                r=false
+//                break;
+//
+//
+//            }
+//        }
+//        else if(s[i]=='}'){
+//            if(!st.empty()&& st.top()=='{'){
+//                st.pop();
+//            }
+//            else{
+//                r=false;
+//                break;
+//
+//            }
+//        }
+//        else if(s[i]==')'){
+//            if(!st.empty()&& st.top()=='(')
+//            {
+//                st.pop();
+//
+//            }
+//            else{
+//                r=false;
+//                break;
+//
+//            }
+//        }
+//
+//    }
+//    if(!st.empty()){
+//        return
+//    }
+//}
 #include<iostream>
 using namespace std;
 #include<stack>
